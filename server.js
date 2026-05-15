76784381c83f88351be9dba5d6dd53a8e3b54ff2
Shreya02092004 <144298192+Shreya02092004@users.noreply.github.com>
2026-05-15 10:59:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import { connectDB } from "./config/db.js";
+
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+dotenv.config();
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+
+// API routes
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
